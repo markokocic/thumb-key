e366aa89b5e8f10b9c948539801bcd56a93b07d7
Marko Kocic <marko@euptera.com>
2026-02-05 11:52:01 +0100

    Cycle through languages when doing swype and return on keyboard change
    Swype and return action on keyboard change now cycles through the list
    of locales defined inside a keyboard definition. This instructs system
    spellchecker to treat input as it belongs to particular language. Useful
    for meltilanguage keyboards.
    
    Keyboard switching always switch to the primary locale for the keyboard.

--- app/src/main/java/com/dessalines/thumbkey/utils/Utils.kt
+++ app/src/main/java/com/dessalines/thumbkey/utils/Utils.kt
@@ -347,6 +347,7 @@ fun performKeyAction(
     onToggleHideLetters: () -> Unit,
     onAutoCapitalize: (enable: Boolean) -> Unit,
     onSwitchLanguage: () -> Unit,
+    onCycleLocale: () -> Unit,
     onChangePosition: ((old: KeyboardPosition) -> KeyboardPosition) -> Unit,
     onKeyEvent: () -> Unit,
 ) {
@@ -1389,6 +1390,10 @@ fun performKeyAction(
 
         KeyAction.SwitchLanguage -> {
             onSwitchLanguage()
+        }
+
+        KeyAction.CycleLocale -> {
+            onCycleLocale()
         }
 
         KeyAction.SwitchIME -> {
